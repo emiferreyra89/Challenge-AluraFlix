9395ee8cdeb81e5198a1ae0cea2f38a040d6638f
emiferreyra89 <ferreyra89@gmail.com>
2024-12-17 18:21:59 -0300

    Desarrolle componentes con estilos globales

---
+++ src/styles/theme.jsx
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primaryBlue: "#2271d1",       // Azul primario (ej: Botones)
+    primaryWhite: "#fff",         // Blanco primario
+    secondaryWhite: "#f5f5f5",    // Blanco secundario
+    secondaryBlack: "#262626",    // Negro secundario (ej: background Header)
+    blueDark: "#03122f",
+    blueLigth: "#6bd1ff",         // Celeste (ej: bordes de tarjetas)
+    greenCards: "#00c86f",
+    yellowCards: "#ffba05",
+  },
+  fonts: {
+    sourceSans: "'Source Sans 3', sans-serif",  // Fuente Botones
+    roboto: "'Roboto', sans-serif",             // Fuente Principal
+  }
+}
