9c990a6aa260944cba6ce0e7b585161cffb80ce8
emiferreyra89 <ferreyra89@gmail.com>
2024-12-19 01:36:40 -0300

    Reacomode componentes de la estructura de ruteo

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,21 +4,19 @@ import { theme } from "./styles/theme.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import FormNewVideo from "./pages/FormNewVideo.jsx";
-import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
 export default function AppRoutes() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <BrowserRouter>
-          <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/form" element={<FormNewVideo />} />
-            </Routes>
-          <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<FormNewVideo />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
